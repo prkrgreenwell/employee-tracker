f6a322fab3025533e409be9bf853712ce46c682f
PARKER GREENWELL <pgreenwell@alpine.local>
2022-12-14 12:19:42 -0700

    started view function

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,15 +6,15 @@ VALUES
 ('Customer Service'),
 ('Warehouse');
 
-INSERT INTO roles (name, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Manager', 100000, NULL),
 ('Salesperson', 60000, 1),
 ('Lead Salesperson', 80000, 1),
 ('Accountant', 65000, 2),
 ('Head Accountant', 75000, 2),
-('Human Resource Representative', 72000, 3),
-('Customer Service Representative', 76000, 4),
+('Human Resource Rep', 72000, 3),
+('Customer Service Rep', 76000, 4),
 ('Warehouse Worker', 45000, 5),
 ('Warehouse Head', 55000, 5);
 
